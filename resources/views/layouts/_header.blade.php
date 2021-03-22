04f012da78f17ce30381c0bc5b567ff1f4e2131b
冉超 <756620245@qq.com>
2021-03-22 08:53:49 +0800

    注册与登录

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,20 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
-    .
-    .
-    .
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
+      Laravel Shop
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav mr-auto">
+
+      </ul>
+
+      <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- 登录注册链接开始 -->
         @guest
